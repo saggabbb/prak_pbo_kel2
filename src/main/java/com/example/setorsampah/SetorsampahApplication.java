9ce0e2e2c290eeddd124ec466f25d7f7d702d8fd
saggabbb <bagasadigia@gmail.com>
2026-05-03 20:04:45 +0700

    initial commit

---
+++ src/main/java/com/example/setorsampah/SetorsampahApplication.java
@@ -0,0 +1,13 @@
+package com.example.setorsampah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetorsampahApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SetorsampahApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/setorsampah/SetorsampahApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.setorsampah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SetorsampahApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
